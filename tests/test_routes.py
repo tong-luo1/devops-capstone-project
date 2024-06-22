aed2540f42de1e765f0c66349ce23176d072570b
tong.luo1 <tong.luo1@ibm.com>
2024-06-22 11:25:04 -0400

    add method not allowed test

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -166,3 +166,7 @@ class TestAccountService(TestCase):
         response = self.client.delete(
             f"{BASE_URL}/{account.id}", content_type="application/json")
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_method_not_allowed(self):
+        resp = self.client.delete(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
